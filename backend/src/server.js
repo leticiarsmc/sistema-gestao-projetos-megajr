7db6b0e1a428ccdfbf432fa91427a342eca7100f
Letícia Cardoso <leticiareginarsmc@gmail.com>
2026-06-10 16:38:26 -0400

    chore: configura execução e rota de saúde da api

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,23 @@
 const express = require('express');
+
 const app = express();
 
 app.use(express.json());
 
-//teste
 app.get('/', (req, res) => {
-  res.json({ message: "rodando" });
+  res.json({ message: 'rodando' });
+});
+
+//rota de saude
+app.get('/health', (req, res) => {
+  res.json({
+    status: 'ok',
+    message: 'rodando',
+  });
 });
 
 const PORT = 3000;
+
 app.listen(PORT, () => {
-  console.log(` servidor rodando na porta ${PORT}`);
+  console.log(`servidor rodando na porta ${PORT}`);
 });
